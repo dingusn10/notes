54ea0d61206d87d5acfadf3f70039b8fdb763fee
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-18 17:19:06 +0000

    fixed github provider import

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,7 +4,7 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import GITHUBProvider from "next-auth/providers/GITHUB";
+import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
