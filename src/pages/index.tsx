927c814a89aedc6160e467c196afc6f663069563
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-18 20:24:32 +0000

    added all basic functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { Header } from "../components/header";
+import { Header } from "../components/Header";
 import { useSession } from "next-auth/react";
 import { api, type RouterOutputs } from "~/utils/api";
 import { useState } from "react";
+import { NoteEditor } from "~/components/NoteEditor";
+import { NoteCard } from "~/components/NoteCard";
 
 
 const Home: NextPage = () => {
@@ -46,6 +48,27 @@ const Content: React.FC = () => {
     },
   });
 
+  const { data: notes, refetch: refetchNotes } = api.note.getAll.useQuery(
+    {
+      topicId: selectedTopic?.id ?? "",
+    },
+    {
+      enabled: sessionData?.user !== undefined && selectedTopic !== null,
+    }
+  );
+
+  const createNote = api.note.create.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
+  });
+
+  const deleteNote = api.note.delete.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
+  });
+
   return (
     <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
       <div className="px-2">
@@ -72,7 +95,27 @@ const Content: React.FC = () => {
           ))}
         </ul>
       </div>
-      <div className="col-span-3"></div>
+      <div className="col-span-3">
+        <div>
+          {notes?.map((note) => (
+            <div className="mt-5" key={note.id}>
+              <NoteCard
+                note={note}
+                onDelete={() => void deleteNote.mutate({ id: note.id })}
+                />
+                </div>
+          ))}
+        </div>
+        <NoteEditor 
+        onSave={({ title, content }) => {
+          void createNote.mutate({
+            title,
+            content,
+            topicId: selectedTopic?.id ?? "",
+          });
+        }}
+        />
+      </div>
     </div>
   );
 };
